7ee2e6828615823bfb873802db74a7e65e0e2938
nguyencaothai6919 <nguyencaothai6919@gmail.com>
2022-10-03 14:01:07 +0700

    something wrong

---
+++ src/main/java/com/thainguyen/admin/ThainguyenApplication.java
@@ -0,0 +1,13 @@
+package com.thainguyen.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThainguyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThainguyenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thainguyen/admin/ThainguyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thainguyen.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThainguyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
